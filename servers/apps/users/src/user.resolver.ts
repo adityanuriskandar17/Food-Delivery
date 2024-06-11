4ae1317844d4685763520b8f1148c336a1083745
adityanuriskandar17 <iskandargrow@gmail.com>
2024-06-11 23:42:45 +0700

    first_commit

---
+++ servers/apps/users/src/user.resolver.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { BadRequestException } from '@nestjs/common';
+import { Args, Mutation, Resolver, Query, Context } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { RegisterResponse } from './types/user.types';
+import { RegisterDto } from './dto/user.dto';
+import { Response } from 'express';
+import { User } from './entities/user.entity';
+
+@Resolver('User')
+// @UseFilters()
+export class UsersResolver {
+  constructor(private readonly userService: UsersService) {}
+
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+    @Context() context: { res: Response },
+  ): Promise<RegisterResponse> {
+    if (!registerDto.name || !registerDto.email || !registerDto.password) {
+      throw new BadRequestException('Please fill the all fields');
+    }
+    const user = await this.userService.register(registerDto, context.res);
+
+    return { user };
+  }
+
+  @Query(() => [User])
+  async getUsers() {
+    return this.userService.getUsers();
+  }
+}
---
+++ servers/apps/users/src/users.service.ts
@@ -0,0 +1,50 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
+import { PrismaService } from '../../../prisma/Prisma.service';
+import { Response } from 'express';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // refgister user
+  async register(registerDto: RegisterDto, response: Response) {
+    const { name, email, password } = registerDto;
+    const isEmailExist = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (isEmailExist) {
+      throw new BadRequestException('User already exist with this email!');
+    }
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    return { user, response };
+  }
+  // Login Service
+  async Login(LoginDto: LoginDto) {
+    const { email, password } = LoginDto;
+    const user = {
+      email,
+      password,
+    };
+    return user;
+  }
+  // Get All Users Service
+  async getUsers() {
+    return this.prisma.user.findMany({});
+  }
+}
